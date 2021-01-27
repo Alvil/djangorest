2de0b5c890916f2905ba4f01d8e0b99b225a7fd4
Alvil <alvil.balbuena@gmail.com>
2021-01-27 15:13:54 +0800

    Added import needed for refactoring.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,4 +1,7 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 from django.http import HttpResponse
 from django.http import JsonResponse
