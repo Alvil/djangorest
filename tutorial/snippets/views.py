e1c774224e620ff5db4dbb9e7323f9f339c0efa2
Alvil <alvil.balbuena@gmail.com>
2021-01-29 15:05:11 +0800

    Changed SnippetDetail to genericview.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
 from rest_framework import status
 from rest_framework.response import Response
@@ -18,32 +19,9 @@ class SnippetList(ListCreateAPIView):
     serializer_class = SnippetSerializer
 
 
-class SnippetDetail(APIView):
+class SnippetDetail(RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update, or delete a snippet
     """
-
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
