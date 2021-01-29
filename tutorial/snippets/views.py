f75cac8636688e69fe423bd2e973b5741c6cec6e
Alvil <alvil.balbuena@gmail.com>
2021-01-29 15:02:55 +0800

    SnippetList to generic.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 
+from rest_framework.generics import ListCreateAPIView
 
 from rest_framework import status
 from rest_framework.response import Response
@@ -9,22 +10,12 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(APIView):
+class SnippetList(ListCreateAPIView):
     """
     List all code snippets, or create a new snippet
     """
-
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
 
 class SnippetDetail(APIView):
