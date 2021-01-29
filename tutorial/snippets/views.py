a0046c2fb3d00ae901b95873ba881f247ff25d83
Alvil <alvil.balbuena@gmail.com>
2021-01-29 14:51:15 +0800

    Remove unnecessary imports on views.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,11 +1,9 @@
 from django.http import Http404
 
-from rest_framework.views import APIView
 
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
