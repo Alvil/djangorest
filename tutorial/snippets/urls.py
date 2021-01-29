579b32e2cb1fd0b65d5c5796e301e4bdb0df4dc9
Alvil <alvil.balbuena@gmail.com>
2021-01-29 14:07:23 +0800

    Add format to urls.

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from .views import snippet_detail
 from .views import snippet_list
 
@@ -7,3 +9,5 @@ urlpatterns = [
     path('snippets/', snippet_list, name='snippet-list'),
     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
