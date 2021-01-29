eb8125965f9827218ffad38750e1c22c9a517e79
Alvil <alvil.balbuena@gmail.com>
2021-01-29 14:49:49 +0800

    Import correct view to urls.

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import snippet_detail
+from .views import SnippetDetail
 from .views import SnippetList
 
 urlpatterns = [
     path('snippets/', SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
