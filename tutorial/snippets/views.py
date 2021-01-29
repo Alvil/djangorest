5d409697d4f0394e4e541ddd8746d726d230dde0
Alvil <alvil.balbuena@gmail.com>
2021-01-29 14:21:57 +0800

    Minor typo.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -17,7 +17,7 @@ class SnippetList(APIView):
     """
 
     def get(self, request, format=None):
-        snippet = Snippet.objects.all()
+        snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
