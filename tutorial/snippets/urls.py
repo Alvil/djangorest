378c30f13162268c6949c80e636ff76ea1961ae3
Alvil <alvil.balbuena@gmail.com>
2021-02-03 12:46:35 +0800

    Add url for api_root.

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import SnippetDetail
-from .views import SnippetList
-from .views import UserDetail
-from .views import UserList
+from snippets.views import api_root
+from snippets.views import SnippetDetail
+from snippets.views import SnippetList
+from snippets.views import UserDetail
+from snippets.views import UserList
 
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('snippets/', SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
     path('users/', UserList.as_view(), name='user-list'),
