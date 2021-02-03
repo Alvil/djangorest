9f65ed3a8ec5cc88b9e17d39014411a2d9816847
Alvil <alvil.balbuena@gmail.com>
2021-02-03 13:25:44 +0800

    Modify urls.

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,22 +1,40 @@
 from django.urls import path
 
+from rest_framework.renderers import StaticHTMLRenderer
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets.views import api_root
-from snippets.views import SnippetDetail
-from snippets.views import SnippetList
-from snippets.views import SnippetHighlight
-from snippets.views import UserDetail
-from snippets.views import UserList
+from snippets.views import SnippetViewSet
+from snippets.views import UserViewSet
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 urlpatterns = [
     path('', api_root, name='api-root'),
-    path('snippets/', SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
     path('snippets/<int:pk>/highlight/',
-         SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('users/', UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+         snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
