4b0e32bb8cebfcae23415dddb424531eedb4e94f
Alvil <alvil.balbuena@gmail.com>
2021-02-03 13:39:41 +0800

    Cleaned imports on urls.py.

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from django.urls import include
 
 from rest_framework.routers import DefaultRouter
-from rest_framework.renderers import StaticHTMLRenderer
-from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets.views import SnippetViewSet
 from snippets.views import UserViewSet
